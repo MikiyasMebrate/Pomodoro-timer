deccab46ea92a3253cfba698edbbb4b83d37fec6
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-10-01 19:32:10 +0300

    menu updated

--- notification.js
+++ notification.js
@@ -1,4 +1,5 @@
 const notifier = require('node-notifier');
+const { resolve } = require('path');
 //save history in document 
 
 
@@ -10,64 +11,126 @@ const readInput = readline.createInterface({
   });
 
 
-const displayQuestion = (query) => {
-  return new Promise((resolve, reject) => {
-    readInput.question(query, (answer) => {
-      resolve(answer);
+  const displayQuestion = (query) => {
+    return new Promise((resolve, reject) => {
+      readInput.question(query, (answer) => {
+        resolve(answer);
+      })
     })
+  }
+  
+  const askQuestion = async (question) =>{
+    let input = await displayQuestion(question)
+  
+    if(input < 1 || isNaN(input)) {
+      console.log("Please enter valid !")
+      return askQuestion(question)
+    }else{
+      return input
+    }
+  
+  }
+
+const displayMenu = async() =>{
+  console.clear()
+  console.log("1. Start Pomodoro")
+  console.log("2. History")
+  console.log("3. Exit")
+  const input = await askQuestion('Enter Options: ')
+
+  if(input == 1){
+    pomodoroMenu()
+  }
+}
+
+
+const pomodoroMenu = async () =>{
+  const breakLength = await askQuestion('Enter break length: ')
+  const breakInterval = await askQuestion('Enter break sec: ')
+  const sessionLength = await askQuestion('Enter session length: ')
+  
+  readInput.close();
+
+
+  for(let interval  = 0; interval < breakLength; interval++){
+    let time = await workTimer(sessionLength, interval+1) // call the work timer function
+    console.log(time)
+
+    if(interval+1 != breakLength){
+      let breakTime = await breakTimer(breakInterval, interval+1) // call the break timer function
+      console.log(breakTime)
+    }
+
+  }
+
+
+  showNotification('Congratulations','Session Completed', 'Hero')  //final notification
+}
+
+
+
+const preTimer = async(message) =>{
+  return new Promise((resolve, reject) => {
+    let sec = 5
+    const timer = setInterval(()=>{
+      console.clear()
+      console.log(`${message} ${sec}`)
+      sec--;
+      if(sec == 0){
+        clearInterval(timer)
+        resolve(true)
+      }
+    },1000)
   })
 }
 
-const askQuestion = async (question) =>{
-  let breakLength = await displayQuestion(question)
 
-  if(breakLength < 1 || isNaN(breakLength)) {
-    console.log("Please enter valid !")
-    return askQuestion(question)
-  }else{
-    return breakLength
+const  workTimer = async (sessionLength, interval) =>{
+  showNotification('Work Time', `Session ${interval} start soon`, 'Funk') 
+  let preTime = await preTimer('Working time stating in ....')
+
+  if(preTime){
+    return new Promise((resolve, reject) => {
+      
+      let startTime = new Date();
+      let countDown = setInterval(() => {
+        let current = new Date();
+        const diff = current - startTime;
+    
+        
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Hours
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)); // Minutes
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000); // Seconds
+    
+        const timer = `Work Timer = ${hours > 9 ? hours : '0' + hours}:${minutes > 9 ? minutes : '0' + minutes}:${seconds > 9 ? seconds : '0' + seconds}`
+    
+        console.clear()
+        console.log(timer)
+    
+    
+        if(seconds == sessionLength){
+          clearInterval(countDown)
+          resolve(`Session ${interval} Over`)
+          return true
+        }
+      }, 1000);
+    })
+    
   }
 
 }
 
-const  workTimer = async (sessionLength, target, interval) =>{
+
+const breakTimer = async (breakInterval, interval) => {
+  showNotification('Break Time', `Break ${interval} start soon`, 'Basso') 
+  let preTime = await preTimer('Break time stating in ....')
 
   return new Promise((resolve, reject) => {
-    let countDown = setInterval(() => {
-      let current = new Date();
-      const diff = current - target;
-  
-      
-      const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Hours
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)); // Minutes
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000); // Seconds
-  
-      const timer = `Work Timer = ${hours > 9 ? hours : '0' + hours}:${minutes > 9 ? minutes : '0' + minutes}:${seconds > 9 ? seconds : '0' + seconds}`
-  
-      console.clear()
-      console.log(timer)
-  
-  
-      if(seconds == sessionLength){
-        clearInterval(countDown)
-        resolve(`Session ${interval} Over`)
-        return true
-      }
-    }, 1000);
-  })
-  
-
-
-
-
-}
-
-
-const breakTimer = async (breakInterval, target, interval) => {
-  return new Promise((resolve, reject) => {
+    
+    let startTime = new Date();
   let countDown = setInterval(() => {
     let current = new Date();
-    const diff = current - target;
+    const diff = current - startTime;
 
     
     const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Hours
@@ -103,40 +166,9 @@ const showNotification = (title, message, sound) =>{
 }
 
 const main = async () => {
-  const breakLength = await askQuestion('Enter break length: ')
-  const breakInterval = await askQuestion('Enter break sec: ')
-  const sessionLength = await askQuestion('Enter session length: ')
-  
-  readInput.close();
-
-  //02:00:00
-  let target = new Date()
-
-  for(let interval  = 0; interval < breakLength; interval++){
-    showNotification('Work Time', `Session ${interval+1} Started`, 'Funk') 
-    let time = await workTimer(sessionLength, target, interval+1) // call the work timer function
-    console.log(time)
-
-
-    target = new Date() // update target by current time
-
-
-    if(interval+1 != breakLength){
-      
-      showNotification('Break Time', `Break ${interval+1} Started`, 'Basso') 
-      let breakTime = await breakTimer(breakInterval, target, interval+1) // call the break timer function
-      console.log(breakTime)
-    }
-
-    target = new Date() // update target by current time
-  }
-
-
-  showNotification('Congratulations','Session Completed', 'Hero')  //final notification
+  displayMenu()
 
   
-  
-
 
 
 }
